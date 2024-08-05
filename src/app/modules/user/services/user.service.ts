f371e763071b3d78e87017b0dcee785cd91fc081
MESSELMII_YAHYA <107892703+MESSELMIyahya@users.noreply.github.com>
2024-08-05 12:40:30 +0100

    Add getUserByEmail method to the userService that finds and returnes user by the given email

--- src/app/modules/user/services/user.service.ts
+++ src/app/modules/user/services/user.service.ts
@@ -2,7 +2,10 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { hash } from 'bcrypt';
-import { UserInterface } from '../interfaces/user.interface';
+import {
+  UserInterface,
+  UserSchemaDocumentType,
+} from '../interfaces/user.interface';
 import { CreateUserInterface } from '../interfaces/create-user.interface';
 
 @Injectable()
@@ -11,11 +14,11 @@ export class UserService {
   constructor(@InjectModel('Users') private UserModel: Model<UserInterface>) {}
 
   // Create User
-  async createUser(user: CreateUserInterface) : Promise<CreateUserInterface> {
-    // verify if the email is taken 
+  async createUser(user: CreateUserInterface): Promise<CreateUserInterface> {
+    // verify if the email is taken
     const isEmailTaken = await this.IsEmailTaken(user.email);
-    if(isEmailTaken){
-      throw new HttpException("Email is already taken",HttpStatus.CONFLICT);
+    if (isEmailTaken) {
+      throw new HttpException('Email is already taken', HttpStatus.CONFLICT);
     }
     try {
       // encrypting the password
@@ -39,25 +42,41 @@ export class UserService {
       });
       //   creating the user
       const res = await newUser.save();
-      
+
       return {
-        username:res.profileInfo.username,
-        email:res.profileInfo.email,
-        fullName:res.profileInfo.fullName,
-        password:res.access.password,
+        username: res.profileInfo.username,
+        email: res.profileInfo.email,
+        fullName: res.profileInfo.fullName,
+        password: res.access.password,
       };
     } catch (err) {
-        throw new HttpException("Server Error",HttpStatus.INTERNAL_SERVER_ERROR)
+      throw new HttpException('Server Error', HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
-  // Is email taken
-  private async IsEmailTaken (email:string):Promise<boolean> {
-    try{
-      const user = await this.UserModel.findOne({"profileInfo.email":email});
-      return user._id ? true : false;
-    }catch{
-      return false
+  // Get User
+  async getUserByEmail(
+    email: string,
+  ): Promise<UserSchemaDocumentType | undefined> {
+    try {
+      return (
+        (await this.UserModel.findOne({ 'profileInfo.email': email })) ||
+        undefined
+      );
+    } catch {
+      throw new HttpException('Server Error', HttpStatus.INTERNAL_SERVER_ERROR);
     }
-  } 
+  }
+
+  // Private methods
+
+  // Is email taken
+  private async IsEmailTaken(email: string): Promise<boolean> {
+    try {
+      const user = await this.UserModel.findOne({ 'profileInfo.email': email });
+      return user._id ? true : false;
+    } catch {
+      return false;
+    }
+  }
 }
