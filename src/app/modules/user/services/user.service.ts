c5795e56f4e438900ae59c75530d9f069a577695
MESSELMII_YAHYA <107892703+MESSELMIyahya@users.noreply.github.com>
2024-08-05 09:54:26 +0100

    Add the MongooseModel of the userSchema to the userModule and Add userService class the has the createUser methode and it hanlds all the user crud operations and searching

--- src/app/modules/user/services/user.service.ts
+++ src/app/modules/user/services/user.service.ts
@@ -1,4 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { hash } from 'bcrypt';
+import { UserInterface } from '../interfaces/user.interface';
+import { CreateUserInterface } from '../interfaces/create-user.interface';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  // Injecting the Users model
+  constructor(@InjectModel('Users') private UserModel: Model<UserInterface>) {}
+
+  // Create User
+  async createUser(user: CreateUserInterface) : Promise<CreateUserInterface> {
+    // verify if the email is taken 
+    const isEmailTaken = await this.IsEmailTaken(user.email);
+    if(isEmailTaken){
+      throw new HttpException("Email is already taken",HttpStatus.CONFLICT);
+    }
+    try {
+      // encrypting the password
+      const encryptedPassword = await hash(user.password, 10);
+      // the user model
+      const newUser = new this.UserModel({
+        profileInfo: {
+          username: user.username,
+          email: user.email,
+          fullName: user.fullName,
+          pic: '',
+        },
+        access: {
+          role: 'User',
+          password: encryptedPassword,
+          canPost: false,
+          verified: false,
+        },
+        courses: [],
+        postedCourses: [],
+      });
+      //   creating the user
+      const res = await newUser.save();
+      
+      return {
+        username:res.profileInfo.username,
+        email:res.profileInfo.email,
+        fullName:res.profileInfo.fullName,
+        password:res.access.password,
+      };
+    } catch (err) {
+        throw new HttpException("Server Error",HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+  }
+
+  // Is email taken
+  private async IsEmailTaken (email:string):Promise<boolean> {
+    try{
+      const user = await this.UserModel.findOne({"profileInfo.email":email});
+      return user._id ? true : false;
+    }catch{
+      return false
+    }
+  } 
+}
--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './services/user.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './schemas/user.schema';
 
 @Module({
-  providers: [UserService]
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Users',
+        schema: UserSchema,
+      },
+    ]),
+  ],
+  providers: [UserService],
+  exports:[UserService],
 })
 export class UserModule {}
