75b30081147a36781c6bf0c80f9b4ca47f757c10
MESSELMII_YAHYA <107892703+MESSELMIyahya@users.noreply.github.com>
2024-08-05 09:47:16 +0100

    Add the MongooseModule to the app module and use the Cookie-parser pakage and change the port

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoreModule } from 'src/core/core.module';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
-  imports: [CoreModule,AuthModule,UserModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI, { dbName: 'LMS',onConnectionCreate:()=>{console.log("Database Connected")}}),
+    CoreModule,
+    UserModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
