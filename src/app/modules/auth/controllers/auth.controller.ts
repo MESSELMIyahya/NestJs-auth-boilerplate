e89952d2158eac765d403ce560facb3769854b66
MESSELMII_YAHYA <107892703+MESSELMIyahya@users.noreply.github.com>
2024-08-05 12:46:16 +0100

    Add generateAccessToken,generateRefershToken,comparePasswords and login methods to the authService that handle the login logic and token generating and password comparing ,Add login method to the authController that handles the login logic with the authService and setting the access,refresh tokens in the response's header and this methods uses the zodValidationPipe

--- src/app/modules/auth/controllers/auth.controller.ts
+++ src/app/modules/auth/controllers/auth.controller.ts
@@ -1,13 +1,17 @@
-import {
-  Body,
-  Controller,
-  Inject,
-  Post,
-} from '@nestjs/common';
+import { Body, Controller, HttpCode, Inject, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { CreateUserInterface } from '../../user/interfaces/create-user.interface';
 import { AuthService } from '../services/auth.service';
 import { ZodValidationPipe } from 'src/core/pipes/zodValidation.pipe';
-import { RegisterUserValidationSchema, RegisterUserValidationSchemaType } from '../validation/register-user.schema';
+import {
+  RegisterUserValidationSchema,
+  RegisterUserValidationSchemaType,
+} from '../validation/register-user.schema';
+import {
+  LoginUserValidationSchema,
+  LoginUserValidationSchemaType,
+} from '../validation/login-user.schema';
+import { CookiesConstants } from '../constants';
 
 @Controller('auth')
 export class AuthController {
@@ -17,11 +21,35 @@ export class AuthController {
   ) {}
 
   // register the user
-  @Post()
+  @Post('register')
   async register(
-    @Body(new ZodValidationPipe(RegisterUserValidationSchema)) body: RegisterUserValidationSchemaType,
+    @Body(new ZodValidationPipe(RegisterUserValidationSchema))
+    body: RegisterUserValidationSchemaType,
   ): Promise<CreateUserInterface> {
     const user = await this.authService.Register(body as CreateUserInterface);
     return user;
+  }
+
+  // login the user
+  @HttpCode(200)
+  @Post('login')
+  async login(
+    @Res({ passthrough: true }) res: Response,
+    @Body(new ZodValidationPipe(LoginUserValidationSchema))
+    body: LoginUserValidationSchemaType,
+  ) {
+    const tokens = await this.authService.Login(
+      body as Required<LoginUserValidationSchemaType>,
+    );
+    if (tokens) {
+      // setting the cookies
+      res.cookie(CookiesConstants.accessToken, tokens.accessToken, {
+        httpOnly: true,
+      });
+      res.cookie(CookiesConstants.refreshToken, tokens.refreshToken, {
+        httpOnly: true,
+      });
+    }
+    return { accessToken: tokens.accessToken };
   }
 }
--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -1,17 +1,22 @@
-import {
-  Inject,
-  Injectable,
-} from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { UserInterface } from '../../user/interfaces/user.interface';
 import { UserService } from '../../user/services/user.service';
 import { CreateUserInterface } from '../../user/interfaces/create-user.interface';
+import { LogicUserInterface } from '../interfaces/login-user.interface';
+import { JwtService } from '@nestjs/jwt';
+import { JwtBodyInterface } from '../interfaces/jwt-body.interface';
+import { compare } from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectModel('Users') private UserModel: Model<UserInterface>,
+
+    @Inject(JwtService)
+    private readonly jwtService: JwtService,
+
     @Inject(UserService)
     private readonly userService: UserService,
   ) {}
@@ -20,5 +25,85 @@ export class AuthService {
   async Register(user: CreateUserInterface): Promise<CreateUserInterface> {
     const newUser = await this.userService.createUser(user);
     return newUser;
+  }
+
+  // Login
+  async Login(body: LogicUserInterface): Promise<{
+    accessToken: string;
+    refreshToken: string;
+  }> {
+    // verifying the user
+    const user = await this.userService.getUserByEmail(body.email);
+    if (!user)
+      throw new HttpException('No user with this email', HttpStatus.NOT_FOUND);
+
+    // checking the passwords
+    const match = await this.comparePasswords(
+      body.password,
+      user.access.password,
+    );
+    if (!match)
+      throw new HttpException('Password is incorrect', HttpStatus.UNAUTHORIZED);
+
+    try {
+      // generating the tokens
+      const jwtBody: JwtBodyInterface = {
+        id: user._id as never,
+        role: user.access.role,
+        email: user.profileInfo.email,
+        username: user.profileInfo.username,
+        pic: user.profileInfo.pic,
+      };
+
+      const accessToken = await this.generateAccessToken(jwtBody);
+      const refreshToken = await this.generateRefreshToken(jwtBody);
+
+      return {
+        accessToken,
+        refreshToken,
+      };
+    } catch {
+      throw new HttpException('Server Error', HttpStatus.SERVICE_UNAVAILABLE);
+    }
+  }
+
+  // Private Methods
+
+  // Generate access token
+  private generateAccessToken(body: JwtBodyInterface): string {
+    try {
+      const token = this.jwtService.sign(body, {
+        expiresIn: '1h',
+      });
+
+      return token;
+    } catch {
+      throw new HttpException('Server Error', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  // generate refresh token
+  private generateRefreshToken(body: JwtBodyInterface): string {
+    try {
+      const token = this.jwtService.sign(body, {
+        expiresIn: '2days',
+      });
+
+      return token;
+    } catch {
+      throw new HttpException('Server Error', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  // verifying passwords
+  private async comparePasswords(
+    password: string,
+    hashedPassword: string,
+  ): Promise<boolean> {
+    try {
+      return await compare(password, hashedPassword);
+    } catch {
+      return false;
+    }
   }
 }
