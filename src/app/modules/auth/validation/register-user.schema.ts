d2315f72e2f03e759d04546e76cfff0276b4df09
MESSELMII_YAHYA <107892703+MESSELMIyahya@users.noreply.github.com>
2024-08-07 09:41:53 +0100

    Add 'pic' field to the registerUser zod validation schema

--- src/app/modules/auth/validation/register-user.schema.ts
+++ src/app/modules/auth/validation/register-user.schema.ts
@@ -4,6 +4,7 @@ export const RegisterUserValidationSchema = z.object({
   username: z.string().min(4, 'Username must be more then 4 characters'),
   fullName: z.string().min(4, 'FullName must be more then 4 characters'),
   email: z.string().email('Email Is not valid'),
+  pic:z.string().default(''),
   password: z.string().min(8, 'Password must be more then 8 characters'),
 }).required();
 
