ab24b46a1e56bebb20f389bb19a7dcb133ecc5ae
MESSELMII_YAHYA <107892703+MESSELMIyahya@users.noreply.github.com>
2024-08-07 10:03:07 +0100

    Improve the code of this file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
-
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGODB_URI, { dbName: 'LMS',onConnectionCreate:()=>{console.log("Database Connected")}}),
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      dbName: 'LMS',
+      onConnectionCreate: () => {
+        console.log('Database Connected');
+      },
+    }),
     CoreModule,
     UserModule,
     AuthModule,
